abb648222c10e7e4562ef76101ba116beeff0076
Kiara <usr4066@salesianos-lacuesta.net>
2025-05-13 10:32:22 +0100

    feat(divespotList): add DivespotList model with basic properties

---
+++ src/com/ripadbisor/models/DivespotList.java
@@ -0,0 +1,13 @@
+package com.ripadbisor.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DivespotList {
+    private List<Divespot> divespots;
+
+    public DivespotList() {
+        divespots = new ArrayList<>();
+    }
+
+}
