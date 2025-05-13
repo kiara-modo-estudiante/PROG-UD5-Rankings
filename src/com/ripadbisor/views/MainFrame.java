0ae4278b201cfe285e6811d9c56345551b7ae92f
Kiara <usr4066@salesianos-lacuesta.net>
2025-05-13 11:38:05 +0100

    feat(mainFrame): add textbox to display information

--- src/com/ripadbisor/views/MainFrame.java
+++ src/com/ripadbisor/views/MainFrame.java
@@ -4,12 +4,21 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
+    private JTextArea displayArea;
 
     public MainFrame() {
         setTitle("Divespot Manager");
         setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+
+        displayArea = new JTextArea();
+        displayArea.setEditable(false);
+        add(new JScrollPane(displayArea), BorderLayout.CENTER);
+    }
+
+    public void displayDivespotInfo(String info) {
+        displayArea.append(info + "\n");
     }
 
     public static void main(String[] args) {
